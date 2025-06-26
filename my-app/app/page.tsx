75d67ac88079f645ede6096b432261ab397b9b66
xdsilnia <mgpersonalmail2001@gmail.com>
2025-06-26 19:19:01 +0200

    App creation

---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <h1 className="text-4xl font-bold">Mateusz Gąsiorek</h1>
+      <h1 className="font-semibold">122940</h1>
+    </main>
+  );
+}
